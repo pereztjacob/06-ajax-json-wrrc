3b33195e7f60ec6a0ae5cd58d35bcf90aa2a97af
G.Provost <grace.g.provost@gmail.com>
2017-11-30 15:48:25 -0800

    stretch goal

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -12,7 +12,7 @@ function Article (rawDataObj) {
 // REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
-// COMMENT: Why isn't this method written as an arrow function?
+// : Why isn't this method written as an arrow function?
 // This method is not written as an arrow function because it contains 'this'
 Article.prototype.toHtml = function() {
     const template = Handlebars.compile($('#article-template').text());
@@ -32,7 +32,7 @@ Article.prototype.toHtml = function() {
 
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
-// COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs? Where did our forEach loop that looped through all articles and called .toHtml() move to?
+// COMMENT DONE: Where is this function called? What does 'rawData' represent now? How is this different from previous labs? Where did our forEach loop that looped through all articles and called .toHtml() move to?
 // Article.loadAll is called in Article.fetchAll, rawData is the retrieved data from the server. This is different because the data is hosted on a server. The forEach loop was moved to articleView.initIndexPage.
 Article.loadAll = rawData => {
     rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
@@ -42,19 +42,52 @@ Article.loadAll = rawData => {
 
 // REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
 Article.fetchAll = () => {
-    // COMMENT: What is this 'if' statement checking for? Where was the rawData set to local storage?
+    // COMMENT DONE: What is this 'if' statement checking for? Where was the rawData set to local storage?
     // The 'if' statement is checking for the existence of rawData within localStorage. rawData is set to localStorage when article.fetchAll is run the first time through.
     if (localStorage.rawData) {
     // REVIEW: When rawData is already in localStorage we can load it with the .loadAll function above and then render the index page (using the proper method on the articleView object).
+        $.ajax({
+            type: 'HEAD',
+            async: true,
+            ifModified: true,
+            url: '/data/hackerIpsum.json',
+            success: function(data, status) {
+                console.log(status)
+                if (status === 'success') {
+                    console.log('cleared' + data);
+                    localStorage.clear();
+                    localStorage.setItem('rawData', data);
+                    Article.loadAll(data);
+                    articleView.initIndexPage();
+                    
+                } else {
+                    console.log('same: ' + status + 'data: ' + data);
+                    Article.loadAll(JSON.parse(localStorage.getItem('rawData')));
+                    articleView.initIndexPage();
+                }
+            },
+            error: function(res, status, err) {
+                console.log(status);
+                console.log(err);
+            }
+        });
+        //     Coded as above, we won't request a new JSON file if we've already downloaded it once. This caching is problematic: if the JSON file is updated (therefore our cache is "invalid"), a new copy won't be requested from the server unless localStorage is cleared. To overcome this, we could use a small and fast AJAX request with a type of HEAD, to request just the headers, and not the contents of the file. The HEAD response will contain a key called "eTag". The value of the eTag header is calculated based on the contents of the file. So if a new article is added (or an existing one is edited even slightly), the eTag will be different.
+        // If we cache the eTag, then we can compare our cached version of it with a new eTag check, to determine if we need to get the whole file or not.
+        // This can introduce some synchronicity issues, so we'll need to carefully control what methods are calling back to what.
+        // Hint: You'll be able to see everything the server returns, if your AJAX success function looks something like this:
 
-    //TODO: This function takes in an argument. What do we pass in to loadAll()?
-        Article.loadAll(JSON.parse(localStorage.getItem('rawData')));
+        // function(data, message, xhr) {
+        //   console.log(xhr);
+        // }
 
-    //TODO: What method do we call to render the index page?
-        articleView.initIndexPage();
+        //TODO DONE: This function takes in an argument. What do we pass in to loadAll()?
+        // Article.loadAll(JSON.parse(localStorage.getItem('rawData')));
+
+        //TODO DONE: What method do we call to render the index page?
+        // 
 
     } else {
-    // TODO: When we don't already have the rawData:
+    // TODO DONE: When we don't already have the rawData:
     // - we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?)
     // - we need to cache it in localStorage so we can skip the server call next time
     // - we then need to load all the data into Article.all with the .loadAll function above
@@ -65,7 +98,8 @@ Article.fetchAll = () => {
             articleView.initIndexPage();
         });
 
-    // COMMENT: Discuss the sequence of execution in this 'else' conditional. Why are these functions executed in this order?
+    // COMMENT DONE: Discuss the sequence of execution in this 'else' conditional. Why are these functions executed in this order?
     // line 62 retrieves the data from the server and stores in in 'res', 63 sets 'res' to localStorage on the client, 64 calls the loadAll function which creates a new Article object, and 65 appends that Article object to the html.
     }
+
 };
